fba4f0ce2a5543a4aa29f924829a307dbfb954d0
Wiraphat Kwankaew <wiraphat.k@hotmail.com>
2026-03-13 01:40:44 +0100

    Meal_plan

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,6 +1,5 @@
 class MealPlan < ApplicationRecord
   belongs_to :user
   has_many :recipes, dependent: :destroy
-  has_one_attached :photo
-  validates :meal, presence: true
+  validates :meal, :date, :meal_type, presence: true
 end
