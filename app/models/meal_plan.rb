c111d72d5a27510c78f68aeba217f304c50ba44f
Wiraphat Kwankaew <wiraphat.k@hotmail.com>
2026-03-13 01:10:57 +0100

    Meal_plan feature

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,2 +1,6 @@
 class MealPlan < ApplicationRecord
+  belongs_to :user
+  has_many :recipes, dependent: :destroy
+  has_one_attached :photo
+  validates :meal, presence: true
 end
--- db/migrate/20260312173749_create_meal_plans.rb
+++ db/migrate/20260312173749_create_meal_plans.rb
@@ -3,6 +3,7 @@ class CreateMealPlans < ActiveRecord::Migration[7.1]
     create_table :meal_plans do |t|
       t.date :date
       t.string :meal_type
+      t.references :user, null:false, foreign_key: true
 
       t.timestamps
     end
