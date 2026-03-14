8b931740fac5ee3b65c720ff7b50ccc06fb26e4d
afaf kharsa <72041457+Afafkharsa@users.noreply.github.com>
2026-03-14 13:49:58 +0400

    fix migration order

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_13_190326) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_13_181132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "meal_plans", force: :cascade do |t|
     t.date "date"
     t.string "meal_type"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "meal"
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_meal_plans_on_user_id"
   end
 
@@ -31,9 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_13_190326) do
     t.text "keywords"
     t.integer "calories"
     t.text "allergens"
+    t.bigint "meal_plan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "meal_plan_id", null: false
     t.index ["meal_plan_id"], name: "index_recipes_on_meal_plan_id"
   end
 
