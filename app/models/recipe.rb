792326cf3fe1998d392c1f2504e4e92174089a70
Wiraphat Kwankaew <wiraphat.k@hotmail.com>
2026-03-20 19:57:20 +0100

    Add Active storage

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :meal_plan, optional: true
   # has_many :chats, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :ingredients, presence: true
