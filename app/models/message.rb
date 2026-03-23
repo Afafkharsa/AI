aa302e85ec58e3877be180158425bc7fe7f20ccf
*Marie-Laure Seguin <mlseguin@gmail.com>
2026-03-23 14:16:44 +0100

    finishing validations in message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
-  belongs_to :chat
   MAX_USER_MESSAGES = 10
+
+  belongs_to :chat
+
+  validate :user_message_limit, if: -> { role=="user" }
+
+  private
+
+  def user_message_limit
+    if chat.messages.where(role= "user").count >= Message::MAX_USER_MESSAGES
+      errors.add(:content, "You can only send #{Message::MAX_USER_MESSAGES} messages per chat")
+    end
+
+  end
+
 end
