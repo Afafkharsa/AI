b4e07b3d2a7df1b19d6d13586218ae5daf052fd8
*Marie-Laure Seguin <mlseguin@gmail.com>
2026-03-23 19:52:46 +0100

    typo rectified'

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   MAX_USER_MESSAGES = 10
 
   belongs_to :chat
+  after_create_commit :broadcast_append_to_chat
 
   validate :user_message_limit, if: -> { role == "user" }
   validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
@@ -12,6 +13,10 @@ class Message < ApplicationRecord
     if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
       errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
     end
+  end
+
+  def broadcast_append_to_chat
+    broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
   end
 
 end
