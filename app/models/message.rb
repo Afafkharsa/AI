1e4ddde2dec91394333fa3725cd1818d18ac862c
*Marie-Laure Seguin <mlseguin@gmail.com>
2026-03-23 19:31:16 +0100

    validation error rectifying

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,16 +3,15 @@ class Message < ApplicationRecord
 
   belongs_to :chat
 
-  validate :user_message_limit, if: -> { role=="user" }
+  validate :user_message_limit, if: -> { role == "user" }
   validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
 
   private
 
   def user_message_limit
-    if chat.messages.where(role= "user").count >= Message::MAX_USER_MESSAGES
-      errors.add(:content, "You can only send #{Message::MAX_USER_MESSAGES} messages per chat")
+    if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+      errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
     end
-
   end
 
 end
